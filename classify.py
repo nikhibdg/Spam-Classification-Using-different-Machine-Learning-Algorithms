f7dd70742f8f9c42a3a53580a5f2d6b08ee12afc
nikhilbdg <nikhilbdg@gmail.com>
2017-05-24 18:54:12 +0530

    pep8 complied the code

--- classify.py
+++ classify.py
@@ -7,7 +7,6 @@ import sklearn.svm
 import sklearn.naive_bayes
 import sklearn.neighbors
 import sklearn.metrics
-from sklearn.utils import shuffle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
@@ -25,7 +24,8 @@ def preprocess(data):
     """
 
     final_message =\
-    data.strip("Subject").strip(":").strip("\n").strip("\r").strip("re").strip("-")
+            data.strip("Subject").strip(":").strip("\n").strip("\r").strip("re").strip("-")
+
     return final_message
 
 
@@ -40,9 +40,8 @@ def get_features(messages):
     X_count_vec = vectorizer.fit_transform(messages)
     transformer = TfidfTransformer(smooth_idf=False)
     X = transformer.fit_transform(X_count_vec)
+
     return X
-
-
 
 
 def read_data(curr_dir):
@@ -53,8 +52,6 @@ def read_data(curr_dir):
 
     """
 
-    ham_data = {}
-    spam_data = {}
     messages_list = []
     temp_list = []
     messages = []
@@ -66,7 +63,6 @@ def read_data(curr_dir):
         with open(file_item, "r") as fp:
             ham_message = preprocess(fp.readline())
             messages_list.append({ham_message: 1})
-
 
     # Reading Spam data
     spam_folder = glob.glob(curr_dir + "/dataset/spam" + "/*.txt")
@@ -81,11 +77,12 @@ def read_data(curr_dir):
 
     random.shuffle(messages_list)
     for item in messages_list:
-        for key,value in item.items():
+        for key, value in item.items():
             messages.append(key)
             labels.append(value)
 
     return messages, labels
+
 
 def test_classifier_nb(X_train, X_test, y_train, y_test, nb_clf):
     """
@@ -99,14 +96,15 @@ def test_classifier_nb(X_train, X_test, y_train, y_test, nb_clf):
     nb_clf.fit(X_train, y_train)
     y_predicted = nb_clf.predict(X_test)
 
-    accuracy = accuracy_score(y_predicted,y_test)
+    accuracy = accuracy_score(y_predicted, y_test)
     print("Naive Bayes Accuracy:", accuracy)
     print("Naive Bayes Classification Report\n")
     print(sklearn.metrics.classification_report(y_test, y_predicted,
-    target_names=None))
+                                                target_names=None))
 
     print("Naive Bayes Confusion Matrix\n")
     print(sklearn.metrics.confusion_matrix(y_test, y_predicted))
+
 
 def test_classifier_svm(X_train, X_test, y_train, y_test, svm_clf):
     """
@@ -120,11 +118,11 @@ def test_classifier_svm(X_train, X_test, y_train, y_test, svm_clf):
     svm_clf.fit(X_train, y_train)
     y_predicted = svm_clf.predict(X_test)
 
-    accuracy = accuracy_score(y_predicted,y_test)
+    accuracy = accuracy_score(y_predicted, y_test)
     print("SVM Accuracy:", accuracy)
     print("SVM Classification Report\n")
     print(sklearn.metrics.classification_report(y_test, y_predicted,
-    target_names=None))
+                                                target_names=None))
 
     print("SVM Confusion Matrix\n")
     print(sklearn.metrics.confusion_matrix(y_test, y_predicted))
@@ -142,11 +140,11 @@ def test_classifier_knn(X_train, X_test, y_train, y_test, knn_clf):
     knn_clf.fit(X_train, y_train)
     y_predicted = knn_clf.predict(X_test)
 
-    accuracy = accuracy_score(y_predicted,y_test)
+    accuracy = accuracy_score(y_predicted, y_test)
     print("KNN Accuracy:", accuracy)
     print("KNN Classification Report\n")
     print(sklearn.metrics.classification_report(y_test, y_predicted,
-    target_names=None))
+                                                target_names=None))
 
     print("KNN Confusion Matrix\n")
     print(sklearn.metrics.confusion_matrix(y_test, y_predicted))
@@ -164,11 +162,11 @@ def test_classifier_dtree(X_train, X_test, y_train, y_test, dtree_clf):
     dtree_clf.fit(X_train, y_train)
     y_predicted = dtree_clf.predict(X_test)
 
-    accuracy = accuracy_score(y_predicted,y_test)
+    accuracy = accuracy_score(y_predicted, y_test)
     print("Decision Tree Accuracy:", accuracy)
     print("Decision Classification Report\n")
     print(sklearn.metrics.classification_report(y_test, y_predicted,
-    target_names=None))
+                                                target_names=None))
 
     print("Decision Tree Confusion Matrix\n")
     print(sklearn.metrics.confusion_matrix(y_test, y_predicted))
@@ -186,11 +184,11 @@ def test_classifier_rforest(X_train, X_test, y_train, y_test, rforest_clf):
     rforest_clf.fit(X_train, y_train)
     y_predicted = rforest_clf.predict(X_test)
 
-    accuracy = accuracy_score(y_predicted,y_test)
+    accuracy = accuracy_score(y_predicted, y_test)
     print("Random Forest Accuracy:", accuracy)
     print("Random Forest Report\n")
     print(sklearn.metrics.classification_report(y_test, y_predicted,
-    target_names=None))
+                                                target_names=None))
 
     print("Random Forest Confusion Matrix\n")
     print(sklearn.metrics.confusion_matrix(y_test, y_predicted))
@@ -208,11 +206,11 @@ def test_classifier_voting(X_train, X_test, y_train, y_test, vote_clf):
     vote_clf.fit(X_train, y_train)
     y_predicted = vote_clf.predict(X_test)
 
-    accuracy = accuracy_score(y_predicted,y_test)
+    accuracy = accuracy_score(y_predicted, y_test)
     print("Voting Classifier Accuracy:", accuracy)
     print("Voting Classifier Report\n")
     print(sklearn.metrics.classification_report(y_test, y_predicted,
-    target_names=None))
+                                                target_names=None))
 
     print("Voting Classifier Confusion Matrix\n")
     print(sklearn.metrics.confusion_matrix(y_test, y_predicted))
@@ -220,13 +218,13 @@ def test_classifier_voting(X_train, X_test, y_train, y_test, vote_clf):
 
 if __name__ == "__main__":
 
-
     n_neighbors = 11
     weights = 'distance'
     curr_dir = dir_path = os.path.dirname(os.path.realpath(__file__))
     if os.path.isdir("dataset"):
         print("Directory Exists, no need of unzipping dataset")
     else:
+        print("Zipping the datasets into two folders Spam and Ham")
         zip_pointer = zipfile.ZipFile("dataset.zip", 'r')
         zip_pointer.extractall(curr_dir)
         zip_pointer.close()
@@ -234,24 +232,26 @@ if __name__ == "__main__":
     messages, labels = read_data(curr_dir)
     features = get_features(messages)
     X_train, X_test, y_train, y_test = train_test_split(features, labels,
-                                       test_size=0.33, random_state=42)
+                                                        test_size=0.33,
+                                                        random_state=42)
 
     nb_clf = sklearn.naive_bayes.MultinomialNB()
     svm_clf = sklearn.svm.LinearSVC()
-    knn_clf = sklearn.neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    knn_clf = sklearn.neighbors.KNeighborsClassifier(n_neighbors,
+                                                     weights=weights)
     dtree_clf = DecisionTreeClassifier(random_state=0)
     rforest_clf = RandomForestClassifier(n_estimators=10)
     vote_clf = VotingClassifier(estimators=[('nb', nb_clf), ('svm', svm_clf),
-                           ('knn', knn_clf), ('dt', dtree_clf),
-                           ('rt', rforest_clf)], voting='hard')
+                                            ('knn', knn_clf),
+                                            ('dt', dtree_clf),
+                                            ('rt', rforest_clf)],
+                                voting='hard')
 
+    # testing the classifier
 
-
-	# testing the classifier
-
-    test_classifier_nb(X_train, X_test,y_train, y_test,  nb_clf)
-    test_classifier_svm(X_train, X_test,y_train, y_test,  svm_clf)
-    test_classifier_knn(X_train, X_test,y_train, y_test,  knn_clf)
-    test_classifier_dtree(X_train, X_test,y_train, y_test,  dtree_clf)
-    test_classifier_rforest(X_train, X_test,y_train, y_test, rforest_clf)
-    test_classifier_voting(X_train, X_test,y_train, y_test, vote_clf)
+    test_classifier_nb(X_train, X_test, y_train, y_test,  nb_clf)
+    test_classifier_svm(X_train, X_test, y_train, y_test,  svm_clf)
+    test_classifier_knn(X_train, X_test, y_train, y_test,  knn_clf)
+    test_classifier_dtree(X_train, X_test, y_train, y_test,  dtree_clf)
+    test_classifier_rforest(X_train, X_test, y_train, y_test, rforest_clf)
+    test_classifier_voting(X_train, X_test, y_train, y_test, vote_clf)
